410b9af33ad831d453e68cacfb2d4bced23eb2bf
Billy Kooistra <164672210+wsotuo@users.noreply.github.com>
2025-09-22 04:17:32 +0000

    Added logic.

---
+++ reverse_name.py
@@ -0,0 +1,7 @@
+def main():
+    first_name = input("Enter your first name: ")
+    reversed_name = first_name[::-1]
+    print(f"Your name in reverse is: {reversed_name}")
+
+if __name__ == "__main__":
+    main()
