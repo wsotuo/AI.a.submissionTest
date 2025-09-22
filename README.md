1dd3274f5a61cfa74840db2af6b6ef33f48669e2
Billy Kooistra <164672210+wsotuo@users.noreply.github.com>
2025-09-22 04:52:22 +0000

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AI.a.submissionTest
+# stuff
--- reverse_name.py
+++ reverse_name.py
@@ -2,6 +2,11 @@ def main():
     first_name = input("Enter your first name: ")
     reversed_name = first_name[::-1]
     print(f"Your name in reverse is: {reversed_name}")
+    count()
+
+def count():
+    print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
+    print("Great")
 
 if __name__ == "__main__":
     main()
